9063dd8d3a6acbcfe8b5364bfd58377d23913c81
ErauQsPhan <mrsplol102@gmail.com>
2024-04-17 23:11:21 +0700

    update

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const router = express.Router({mergeParams: true});
+const express = require("express");
+const router = express.Router({ mergeParams: true });
 
-const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
-const { validationReview, isLoggIn, isReviewAuthor } = require('../middleware');
+const catchAsync = require("../utils/catchAsync");
+// const ExpressError = require('../utils/ExpressError');
+const { validationReview, isLoggIn, isReviewAuthor } = require("../middleware");
 
-const Campground = require('../models/campground');
-const Review = require('../models/review');
-const reviews = require('../controllers/reviews')
+// const Campground = require('../models/campground');
+// const Review = require('../models/review');
+const reviews = require("../controllers/reviews");
 
+router.post("/", isLoggIn, validationReview, catchAsync(reviews.createReview));
 
-router.post('/', isLoggIn, validationReview, catchAsync(reviews.createReview));
+router.delete(
+  "/:reviewId",
+  isLoggIn,
+  isReviewAuthor,
+  catchAsync(reviews.deleteReview)
+);
 
-router.delete('/:reviewId', isLoggIn, isReviewAuthor, catchAsync(reviews.deleteReview));
-
-
-module.exports = router;  
+module.exports = router;
