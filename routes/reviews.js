7bb05fb13db48ea3cac2e96c3156f6e8a67a0fdd
ErauQsPhan <mrsplol102@gmail.com>
2024-04-18 23:41:27 +0700

    update

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,11 +2,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 const catchAsync = require("../utils/catchAsync");
-// const ExpressError = require('../utils/ExpressError');
 const { validationReview, isLoggIn, isReviewAuthor } = require("../middleware");
-
-// const Campground = require('../models/campground');
-// const Review = require('../models/review');
 const reviews = require("../controllers/reviews");
 
 router.post("/", isLoggIn, validationReview, catchAsync(reviews.createReview));
