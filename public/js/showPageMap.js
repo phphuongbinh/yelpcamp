28758941252b4b0c9f01d7f67fe04791c8c6b0ce
ErauQsPhan <mrsplol102@gmail.com>
2024-04-19 22:50:53 +0700

    update

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,22 +1,21 @@
-console.log(camp.geometry.coordinates)
+console.log(camp.geometry.coordinates);
 mapboxgl.accessToken = mapToken;
-  const map = new mapboxgl.Map({
-    container: "map", // container ID
-    style: "mapbox://styles/mapbox/streets-v11", // style URL
-    center: camp.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9, // starting zoom
-    projection: "globe", // display the map as a 3D globe
-  });
-  map.on("style.load", () => {
-    map.setFog({}); // Set the default atmosphere style
-  });
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: camp.geometry.coordinates,
+  zoom: 9,
+  projection: "globe",
+});
+map.on("style.load", () => {
+  map.setFog({});
+});
 
 new mapboxgl.Marker()
   .setLngLat(camp.geometry.coordinates)
   .setPopup(
-    new mapboxgl.Popup({offset: 25})
-      .setHTML(
-        `<h3>${camp.title}</h3><p>${camp.location}</p>`
-      )
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${camp.title}</h3><p>${camp.location}</p>`
+    )
   )
   .addTo(map);
